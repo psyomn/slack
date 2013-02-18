d4b43c7be58683cd42b9209f214e86b0a41d93cb
psyomn <lethaljellybean@gmail.com>
2013-02-18 02:13:28 -0500

    First commit of the item class

---
+++ lib/item.rb
@@ -0,0 +1,14 @@
+# Author :: Simon Symeonidis
+# Class that encapsulates functionality of an item.
+# This includes consumable items, and non-consumable items (such
+# as potions and swords, etc). 
+# This can also include other items such as key items.
+class Item
+
+  attr_accessor :id
+
+  def initialize
+  end 
+
+
+end
